c0c836df566c76f555e73878e05dbb870185acde
Nobuta wa Produce <sunshine.h2002@gmail.com>
2016-11-16 14:00:22 -0800

    Still dont know

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,21 +1,34 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
-function one() {
+
+
+var currentShape = "circle"
+function shapes() {
+  var buttonClicked = 0
+  function alertOnlyOnce() {
+    if (buttonClicked == 0) {
+      alert(shapes)
+      buttonClicked = 1
+    }
+  }
   var canvas = document.getElementById("game-board")
-  var circle = document.createElementNS(namespace, "circle")
-  circle.setAttribute("cx", 40)
-  circle.setAttribute("cy", 40)
-  circle.setAttribute("r", 10)
-  circle.setAttribute("fill" , "blue")
-  canvas.appendChild(circle)
-}
-function two() {
-  var canvas = document.getElementById("game-board")
-  var circle = document.createElementNS(namespace, "circle")
-  circle.setAttribute("cx", 100)
-  circle.setAttribute("cy", 40)
-  circle.setAttribute("r", 10)
-  circle.setAttribute("fill" , "green")
-  canvas.appendChild(circle)
+  if (currentShape == "circle") {
+    var circle = document.createElementNS(namespace, "circle")
+    circle.setAttribute("cx", 40)
+    circle.setAttribute("cy", 40)
+    circle.setAttribute("r", 20)
+    circle.setAttribute("fill" , "blue")
+    canvas.appendChild(circle)
+    currentShape = "rect"
+  } else {
+    var rect = document.createElementNS(namespace, "rect")
+    rect.setAttribute("x", "80")
+    rect.setAttribute("y", "20")
+    rect.setAttribute("width", "40")
+    rect.setAttribute("height", "40")
+    rect.setAttribute("fill", "green")
+    canvas.appendChild(rect)
+    currentShape = "circle"
+  }
 }
