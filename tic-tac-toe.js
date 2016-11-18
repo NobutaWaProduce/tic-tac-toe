33af3425534bf7dec97d96632f6e61bd808c78ad
Nobuta wa Produce <sunshine.h2002@gmail.com>
2016-11-18 14:00:44 -0800

    I FINALLY UNDERSTAND WHAT TO DO

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,12 +5,39 @@ var namespace = "http://www.w3.org/2000/svg"
 
 
 var turn = "player1"
-function squareTwo(){
+function one(){
   if (turn == "player1"){
     var currentShape = "circle"
      var canvas = document.getElementById("game-board")
     var circle = document.createElementNS(namespace, "circle")
     circle.setAttribute("cx", 40)
+    circle.setAttribute("cy", 40)
+    circle.setAttribute("r", 20)
+    circle.setAttribute("fill" , "blue")
+    canvas.appendChild(circle)
+    currentShape = "rect"
+    turn = "player2"
+  }else{
+    var currentShape = "rect"
+     var canvas = document.getElementById("game-board")
+    var rect = document.createElementNS(namespace, "rect")
+    rect.setAttribute("x", "20")
+    rect.setAttribute("y", "20")
+    rect.setAttribute("width", "40")
+    rect.setAttribute("height", "40")
+    rect.setAttribute("fill", "green")
+    canvas.appendChild(rect)
+    currentShape = "circle"
+    turn = "player1"
+  }
+}
+
+function two(){
+  if (turn == "player1"){
+    var currentShape = "circle"
+     var canvas = document.getElementById("game-board")
+    var circle = document.createElementNS(namespace, "circle")
+    circle.setAttribute("cx", 100)
     circle.setAttribute("cy", 40)
     circle.setAttribute("r", 20)
     circle.setAttribute("fill" , "blue")
